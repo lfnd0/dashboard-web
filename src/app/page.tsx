3e9d05b4c0c60723a58824096136fd05ce9135db
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-04-20 17:04:55 -0300

    Dark Mode no Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,24 @@
 
 export default function Home() {
   return (
-    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1
-        className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex "
-      >
-        Dashboard
-      </h1>
+    <div className="p-8 bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-slate-100 h-screen flex flex-col items-center text-center">
+      <div className="max-w-2xl">
+        <h1
+          className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center"
+        >
+          Dashboard: a simple dashboard for resource management.
+        </h1>
 
-      <h2>A simple dashboard for resource management.</h2>
+        <p className="mt-4 dark:text-slate-400">
+          A web project using Next.js and Tailwind CSS
+        </p>
 
-      <p>A web project using Next.js and Tailwind CSS</p>
-
-      <button
-        disabled
-        className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
-      >
-        Sign in
-      </button>
+        <button
+          className="bg-sky-500 dark:bg-sky-400 dark:text-sky-950 text-white px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
+        >
+          Sign in
+        </button>
+      </div>
     </div>
   );
 }
