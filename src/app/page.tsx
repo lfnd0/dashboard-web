e497055e87019fdd7375052a87dc3e6fd649fd8d
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-04-20 17:36:20 -0300

    Configurando ESLint e Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-
 export default function Home() {
   return (
-    <div className="p-8 bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-slate-100 h-screen flex flex-col items-center text-center">
-      {/* <div className="max-w-[700px]"> */}
+    <div className="flex h-screen flex-col items-center bg-slate-50 p-8 text-center text-slate-900 dark:bg-slate-900 dark:text-slate-100">
       <div className="max-w-app">
-        <h1
-          className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center"
-        >
+        <h1 className="flex items-center text-3xl font-bold sm:text-5xl lg:text-6xl">
           Dashboard: a web project using Next.js and Tailwind CSS
         </h1>
 
@@ -14,12 +10,10 @@ export default function Home() {
           A simple dashboard for resource management.
         </p>
 
-        <button
-          className="bg-sky-500 dark:bg-sky-400 dark:text-sky-950 text-white px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
-        >
+        <button className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-medium text-white enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-sky-400 dark:text-sky-950">
           Sign in
         </button>
       </div>
     </div>
-  );
+  )
 }
