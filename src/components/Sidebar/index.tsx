e00b4e2c4ce4e1f0e5c3b3045664488d37e3be32
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-04-23 00:48:32 -0300

    Pattern de Composição

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -9,6 +9,7 @@ import {
   SquareStack,
   User,
 } from 'lucide-react'
+import { InputControl, InputPrefix, InputRoot } from '../Input'
 import { Logo } from './Logo'
 import { NavItem } from './NavItem'
 import { Profile } from './Profile'
@@ -19,14 +20,12 @@ export function Sidebar() {
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="mx-1 flex w-full items-center  gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-
-        <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-        />
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl placeholder="Search" />
+      </InputRoot>
 
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
