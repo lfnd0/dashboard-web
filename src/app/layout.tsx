43645c5cb0973690b4ef5a680e0c05670d6e614f
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-04-22 22:17:41 -0300

    Widget de Espaço Usado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Sidebar } from '@/components/Sidebar/inde'
+import { Sidebar } from '@/components/Sidebar'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
