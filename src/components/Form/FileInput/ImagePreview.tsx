1c749ff10a6355c74a5e495d8b2d2b6162f47e52
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-05-01 19:48:32 -0300

    Context e Preview da Imagem

--- src/components/Form/FileInput/ImagePreview.tsx
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -1,9 +1,36 @@
+'use client'
+
 import { User } from 'lucide-react'
+import Image from 'next/image'
+import { useMemo } from 'react'
+import { useFileInput } from './Root'
 
 export function ImagePreview() {
-  return (
-    <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
-      <User className="h-8 w-8 text-violet-500" />
-    </div>
-  )
+  const { files } = useFileInput()
+
+  const previewURL = useMemo(() => {
+    if (!files.length) {
+      return null
+    }
+
+    return URL.createObjectURL(files[0])
+  }, [files])
+
+  if (!previewURL) {
+    return (
+      <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
+        <User className="h-8 w-8 text-violet-500" />
+      </div>
+    )
+  } else {
+    return (
+      <Image
+        src={previewURL}
+        className="h-16 w-16 rounded-full object-cover"
+        height={64}
+        width={64}
+        alt=""
+      />
+    )
+  }
 }
