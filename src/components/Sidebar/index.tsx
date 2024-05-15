f17fdadc8471e4e8aebc02f72529c8cc0cc5b49d
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-05-14 21:37:56 -0300

    Abertura da Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { Content, Root, Trigger } from '@radix-ui/react-collapsible'
 import {
   BarChart,
   CheckSquare,
@@ -5,10 +8,12 @@ import {
   Flag,
   Home,
   LifeBuoy,
+  Menu,
   Search,
   SquareStack,
   User,
 } from 'lucide-react'
+import { Button } from '../Button'
 import { InputControl, InputPrefix, InputRoot } from '../Input'
 import { Logo } from './Logo'
 import { NavItem } from './NavItem'
@@ -17,37 +22,49 @@ import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export function Sidebar() {
   return (
-    <aside className="fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:relative lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
-      <Logo />
+    <Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0">
+      <div className="flex items-center justify-between">
+        <Logo />
+        <Trigger className="lg:hidden" asChild>
+          <Button variant="ghost">
+            <Menu className="h-6 w-6" />
+          </Button>
+        </Trigger>
+      </div>
 
-      <InputRoot>
-        <InputPrefix>
-          <Search className="h-5 w-5 text-zinc-500" />
-        </InputPrefix>
-        <InputControl placeholder="Search" />
-      </InputRoot>
+      <Content
+        forceMount
+        className="flex flex-1 flex-col data-[state=closed]:hidden lg:data-[state=closed]:flex"
+      >
+        <InputRoot>
+          <InputPrefix>
+            <Search className="h-5 w-5 text-zinc-500" />
+          </InputPrefix>
+          <InputControl placeholder="Search" />
+        </InputRoot>
 
-      <nav className="space-y-0.5">
-        <NavItem title="Home" icon={Home} />
-        <NavItem title="Dashboard" icon={BarChart} />
-        <NavItem title="Projects" icon={SquareStack} />
-        <NavItem title="Tasks" icon={CheckSquare} />
-        <NavItem title="Reporting" icon={Flag} />
-        <NavItem title="Users" icon={User} />
-      </nav>
-
-      <div className="mt-auto flex  flex-col gap-6">
         <nav className="space-y-0.5">
-          <NavItem title="Support" icon={LifeBuoy} />
-          <NavItem title="Settings" icon={Cog} />
+          <NavItem title="Home" icon={Home} />
+          <NavItem title="Dashboard" icon={BarChart} />
+          <NavItem title="Projects" icon={SquareStack} />
+          <NavItem title="Tasks" icon={CheckSquare} />
+          <NavItem title="Reporting" icon={Flag} />
+          <NavItem title="Users" icon={User} />
         </nav>
 
-        <UsedSpaceWidget />
+        <div className="mt-auto flex  flex-col gap-6">
+          <nav className="space-y-0.5">
+            <NavItem title="Support" icon={LifeBuoy} />
+            <NavItem title="Settings" icon={Cog} />
+          </nav>
 
-        <div className="h-px bg-zinc-200" />
+          <UsedSpaceWidget />
 
-        <Profile />
-      </div>
-    </aside>
+          <div className="h-px bg-zinc-200" />
+
+          <Profile />
+        </div>
+      </Content>
+    </Root>
   )
 }
